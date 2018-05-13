841ef01c2f9eae8a94c39c5fe02156a1e4f88a17
candy5232 <mylas1121@163.com>
2018-05-13 23:54:03 +0800

    vue-example
    vue-example

---
+++ vue-example/src/views/slider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="slider">
+       我是slider
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "slider"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-example/src/views/study.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      我是study
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "study"
+    }
+</script>
+
+<style scoped>
+
+</style>
